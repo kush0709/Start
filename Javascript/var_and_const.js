aa56bcf9a3e06da3f9a30ef24057d19b287efc0b
kush0709e <kaushal100singh@gmail.com>
2022-08-29 03:05:10 +0530

    update

--- Javascript/var_and_const.js
+++ Javascript/var_and_const.js
@@ -4,8 +4,17 @@
 
     //let & var  -- any value that changes
 
+    const companyName = "ABC corporation"
+     // let follow block scoping . var follow local &functional scoping
 
 
+    let salary = 10000;
+    var empid = 12345;
+    console.log(empid);
+    console.log(salary);
+    var empid = 5; //recaling
+    console.log(empid); //5
+    // let salary = 10000;  // error
 
 
 
