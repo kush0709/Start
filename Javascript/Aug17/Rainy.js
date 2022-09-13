669e63f4a73c9977c367450c58595afda88bf9da
kush0709 <kaushal100singh@gmail.com>
2022-09-14 00:02:55 +0530

    update

--- Javascript/Aug17/Rainy.js
+++ Javascript/Aug17/Rainy.js
@@ -1,10 +1,10 @@
 (function() {
-    let temp = 20;
+    let temp = 26;
     let raining = true;
 
-    if(temp>20 && temp <25 && ! raining == false) {
+    if(temp>=20 && temp <=25 && ! raining == false) {
         console.log("go play out"); 
-    } else if (temp>18 && temp <20 && raining == true) {
+    } else if (temp>=18 && temp <=20 && raining == true) {
         console.log("go play in");
     } else {
         console.log("stay home");
