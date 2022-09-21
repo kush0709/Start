a36bea66ab1cc7ba6e0c8e7a4a0bd91a78c22c1b
kush0709 <kaushal100singh@gmail.com>
2022-09-22 00:10:08 +0530

    update

--- Javascript/Aug19/prism.js
+++ Javascript/Aug19/prism.js
@@ -1,6 +1,15 @@
 (function(){
-
-
-
-    
+    function prism(radius,perimeter,vol) {
+        let semiperimeter = perimeter/2;  //we can do it with direct perimeter as well as we dont need to convert it in semiperimeter
+        let height = vol / (semiperimeter*radius);
+        return height;
+    }
+console.log(prism(3,15,270));
 })();
+
+
+
+
+// // //radius = area/semiperimeter
+// // h = v/a    a = v/h
+// h = 2v / (r*p)
