042d82eee2cd68644da33d46c04a5e683b00bb85
kush0709 <kaushal100singh@gmail.com>
2022-10-04 01:39:11 +0530

    update

---
+++ Javascript/Aug19/lcm.js
@@ -0,0 +1,15 @@
+(function(){
+    function getLcm(num1 , num2){
+        let greater = num1>=num2 ?num1 : num2;
+        while(true){
+            if (greater % num1 == 0  && greater % num2 == 0) {
+                break;
+            }
+            greater++;
+        }
+        return greater;
+    }
+    console.log(getLcm(24,36));
+    console.log(getLcm(5,10));
+
+})();
