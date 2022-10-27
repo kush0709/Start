fd142da8288655f87f3c954c9dfd960e9989aebb
kush0709 <kaushal100singh@gmail.com>
2022-10-27 08:42:53 +0530

    update

--- Javascript/Aug20/recursion.js
+++ Javascript/Aug20/recursion.js
@@ -1,3 +1,8 @@
 (function() {
-
+    function tableMath(table , n) {
+        for(let i = 1; i <= n; i++){
+            console.log(`${table}*${i} is ${table*i}`)
+        }    
+    }
+    tableMath(5,10);
 })();
