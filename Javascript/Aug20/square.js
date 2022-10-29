57ba8f5a20d4fb3bd10d49565f17a7d8cf0c2994
kush0709 <kaushal100singh@gmail.com>
2022-10-29 06:18:41 +0530

    update

--- Javascript/Aug20/square.js
+++ Javascript/Aug20/square.js
@@ -2,3 +2,9 @@ function square(num) {
     return num*num; 
 }
 
+function calculate() {
+    let usrInput = document.getElementById("number").value;
+    let output = square(usrInput);
+    let outputElement = document.getElementById("output");
+    outputElement.value = output;
+}
